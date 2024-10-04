f375423091b2deaa424d33aecc1af4267ecd51ea
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-10-04 16:58:47 +0000

    added user accounts sign in nav and foreign keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "boards#index"
   #get("/", { :controller => "boards", :action => "index" })
 
@@ -43,5 +44,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
+  #root "articles#index"
 end
